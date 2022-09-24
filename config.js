9c99ae1d86da250cd89997b69a8f76ff8823cf36
prabhukingsp <114296607+prabhukingsp@users.noreply.github.com>
2022-09-24 19:15:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,16 +31,16 @@ global.APIKeys = {
 
 // Other
 
-global.alivemsg ="HI I'M  ⚜️𝘋𝘈𝘙𝘒 𝘕𝘌𝘙𝘖 𝘔𝘋⚜️"
+global.alivemsg ="හායි බං ඔයාට විතරයි මාව මතක් උනේ 🙂❤ මම ඔන්ලයින් ඉන්නේ ✊🏻🙂    ⚜️𝚂𝚊𝚙𝚜𝚊𝚛𝚊 𝚙𝚛𝚊𝚋𝚑𝚞⚜️"
                      
-global.alivepic = "https://i.ibb.co/8N6fR4X/Whats-App-Image-2022-09-17-at-21-22-46.jpg" //you logo key 
+global.alivepic = "https://i.ibb.co/0c3f6y4/20220409-103921.jpg" //you logo key 
 
 
-global.ownername = " KAVEESHA " //ur owner name
-global.owner = ['94774579505','']
-global.premium = ['94774579505','94774579505']
+global.ownername = " 𝚂𝚊𝚙𝚜𝚊𝚛𝚊 𝚙𝚛𝚊𝚋𝚑𝚞 " //ur owner name
+global.owner = ['94760020233','']
+global.premium = ['94760020233','94760020233']
 global.botname = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
-global.ownername = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
+global.ownername = '🌹𝚂𝚊𝚙𝚜𝚊𝚛𝚊 𝚙𝚛𝚊𝚋𝚑𝚞🍃'
 global.group1 = 'https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX'
 global.group2 = 'https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX'
 global.packname = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
